87fc9e35a0479204042aa8cd43db850ba14b148f
kgersen@hotmail.com <kgersen@hotmail.com@19936f7e-97bc-11dd-9026-69e4e771c826>
2013-03-20 14:53:00 +0000

    - addced: coredump project

---
+++ trunk/ICE/CoreDump/ObjectDumper.cs
@@ -0,0 +1,161 @@
+﻿//Copyright (C) Microsoft Corporation.  All rights reserved.
+// kgersen = heavily modified to "flatten" and "single line"ing the output
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using IGCLib;
+
+// See the ReadMe.html for additional information
+public class ObjectDumper {
+
+    public static void Write(string prefix, object element)
+    {
+        Write(prefix, element, Console.Out);
+    }
+
+    public static void Write(string prefix, object element, TextWriter log)
+    {
+        ObjectDumper dumper = new ObjectDumper();
+        dumper.writer = log;
+        dumper.WriteObject(prefix, element);
+    }
+
+    TextWriter writer;
+    int pos;
+
+    private ObjectDumper()
+    {
+    }
+
+    private void Write(string s)
+    {
+        if (s != null) {
+            writer.Write(s);
+            pos += s.Length;
+        }
+    }
+
+    private void WriteLine()
+    {
+        writer.WriteLine();
+        pos = 0;
+    }
+
+    private void WriteTab()
+    {
+        Write("  ");
+        while (pos % 8 != 0) Write(" ");
+    }
+
+    private void WriteObject(string prefix, object element)
+    {
+        if (element == null || element is ValueType || element is string || element is TechTreeBitMask) {
+            Write(prefix);
+            WriteValue(element);
+            WriteLine();
+        }
+        else
+        {
+            IEnumerable enumerableElement = element as IEnumerable;
+            if (enumerableElement != null)
+            {
+                foreach (object item in enumerableElement)
+                {
+                    if (item is IEnumerable && !(item is string))
+                    {
+                        Write(prefix);
+                        Write("...");
+                        WriteLine();
+                        WriteObject(prefix, item);
+                    }
+                    else
+                    {
+                        WriteObject(prefix, item);
+                    }
+                }
+            }
+            else
+            {
+                MemberInfo[] members = element.GetType().GetMembers(BindingFlags.Public | BindingFlags.Instance);
+                foreach (MemberInfo m in members)
+                {
+                    FieldInfo f = m as FieldInfo;
+                    PropertyInfo p = m as PropertyInfo;
+                    if (f != null || p != null)
+                    {
+                        Type t = f != null ? f.FieldType : p.PropertyType;
+                        if (t.IsValueType || t == typeof(string))
+                        {
+                            Write(prefix);
+                            Write(m.Name);
+                            Write("=");
+                            WriteValue(f != null ? f.GetValue(element) : p.GetValue(element, null));
+                            WriteLine();
+                        }
+                        else
+                        {
+                            if (typeof(IEnumerable).IsAssignableFrom(t))
+                            {
+                                //Write("..."); WriteLine();
+                            }
+                            else
+                            {
+                                //Write("{ }"); WriteLine();
+                            }
+                        }
+                    }
+                }
+                foreach (MemberInfo m in members)
+                {
+                    FieldInfo f = m as FieldInfo;
+                    PropertyInfo p = m as PropertyInfo;
+                    if (f != null || p != null)
+                    {
+                        Type t = f != null ? f.FieldType : p.PropertyType;
+                        if (!(t.IsValueType || t == typeof(string)))
+                        {
+                            object value = f != null ? f.GetValue(element) : p.GetValue(element, null);
+                            if (value != null)
+                            {
+                                WriteObject(prefix + m.Name + ": ", value);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void WriteValue(object o)
+    {
+        if (o == null) {
+            Write("null");
+        }
+        else if (o is DateTime) {
+            Write(((DateTime)o).ToShortDateString());
+        }
+        else if (o is ValueType || o is string) {
+            Write(o.ToString());
+        }
+        else if (o is TechTreeBitMask)
+        {
+            TechTreeBitMask ttbm = (TechTreeBitMask)o;
+            int idx = 0;
+            foreach (bool b in ttbm.bits)
+            {
+                if (b) WriteValue(" "+idx);
+                idx++;
+            }
+        }
+        else if (o is IEnumerable)
+        {
+            Write("...");
+        }
+        else
+        {
+            Write("{ }");
+        }
+    }
+}
---
+++ trunk/ICE/CoreDump/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IGCLib;
+using System.Reflection;
+
+namespace CoreDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Out.Write("error: no argument");
+                return;
+            }
+            IGCCore core = new IGCCore();
+            core.Load(args[0]);
+            
+            //TODO: fancy print GAs attributes and other array values
+
+            ObjectDumper.Write("Globals-",core.Constants);
+
+            foreach (DataCivilizationIGC civ in core.m_civilizations)
+            {
+                string civid = "Factions-" + civ.name + "(" + civ.civilizationID+ ")-";
+                ObjectDumper.Write(civid,civ);
+            }
+            foreach (DataDevelopmentIGC dev in core.m_developments)
+            {
+                string devid = "Devels-"+dev.name+"("+dev.developmentID+")-";
+                ObjectDumper.Write(devid, dev);
+            }
+            foreach (DataDroneTypeIGC d in core.m_droneTypes)
+            {
+                string did = "Drones-" + d.name + " (" + d.droneTypeID + ")-";
+                ObjectDumper.Write(did, d);
+            }
+            foreach (DataProjectileTypeIGC p in core.m_projectileTypes)
+            {
+                string pid = "Projectiles-p#" + p.projectileTypeID +"-";
+                ObjectDumper.Write(pid, p);
+            }
+            foreach (DataStationTypeIGC s in core.m_stationTypes)
+            {
+                string sid = "Stations-" + s.name + "(" + s.stationTypeID + ")-";
+                ObjectDumper.Write(sid, s);
+            }
+            foreach (DataHullTypeIGC h in core.m_hullTypes)
+            {
+                string hid = "Ships-" + h.name + "(" + h.hullID + ")-";
+                ObjectDumper.Write(hid, h);
+            }
+            foreach (DataPartTypeIGC p in core.m_partTypes)
+            {
+                string pid = "Parts-" + p.name + "(" + p.partID + ")-";
+                ObjectDumper.Write(pid, p);
+            }
+            foreach (DataTreasureSetIGC t in core.m_treasureSets)
+            {
+                string tid = "Treasures-" + t.name + "(" + t.treasureSetID + ")-";
+                ObjectDumper.Write(tid, t);
+            }
+        }
+    }
+}
