b8b4c19234fbec55aa9c17c558844c6d9cf7d003
kgersen <kgersen@hotmail.com>
2024-10-02 18:26:28 +0200

    coredump dropped, renamed igcexport, moved to .net 8.0 c++ projets retargeted

---
+++ igcexport/ObjectDumper.cs
@@ -0,0 +1,174 @@
+﻿//Copyright (C) Microsoft Corporation.  All rights reserved.
+// kgersen = heavily modified to "flatten" and "single line"ing the output
+using IGCLib;
+using System.Collections;
+using System.Reflection;
+
+namespace igcexport;
+
+public class ObjectDumper {
+
+    public static void Write(string prefix, object element)
+    {
+        Write(Console.Out, prefix, element);
+    }
+
+    public static void Write(TextWriter w, string prefix, object element)
+    {
+        ObjectDumper dumper = new();
+        dumper.WriteObject(w,prefix, element);
+    }
+
+    int pos;
+
+    private ObjectDumper()
+    {
+    }
+
+    private void Write(TextWriter w, string? s)
+    {
+        if (s != null) {
+            w.Write(s);
+            pos += s.Length;
+        }
+    }
+
+    private void WriteLine(TextWriter w)
+    {
+        w.WriteLine();
+        pos = 0;
+    }
+
+    private void WriteTab(TextWriter w)
+    {
+        Write(w,"  ");
+        while (pos % 8 != 0) Write(w, " ");
+    }
+
+    private void WriteObject(TextWriter w,string prefix, object element)
+    {
+        if (element == null || element is ValueType || element is string || element is TechTreeBitMask) {
+            Write(w,prefix);
+            WriteValue(w,element);
+            WriteLine(w);
+        }
+        else
+        {
+            if (element is IEnumerable enumerableElement)
+            {
+                foreach (object item in enumerableElement)
+                {
+                    if (item is IEnumerable && item is not string)
+                    {
+                        Write(w,prefix);
+                        Write(w,"...");
+                        WriteLine(w);
+                        WriteObject(w,prefix, item);
+                    }
+                    else
+                    {
+                        WriteObject(w,prefix, item);
+                    }
+                }
+            }
+            else
+            {
+                MemberInfo[] members = element.GetType().GetMembers(BindingFlags.Public | BindingFlags.Instance);
+                foreach (MemberInfo m in members)
+                {
+                    FieldInfo? f = m as FieldInfo;
+                    PropertyInfo? p = m as PropertyInfo;
+                    if (f != null || p != null)
+                    {
+                        Type t = f != null ? f.FieldType : p.PropertyType;
+                        if (t.IsValueType || t == typeof(string))
+                        {
+                            Write(w,prefix);
+                            Write(w,m.Name);
+                            Write(w,"=");
+                            WriteValue(w,f != null ? f.GetValue(element) : p.GetValue(element, null));
+                            WriteLine(w);
+                        }
+                        else
+                        {
+                            if (typeof(IEnumerable).IsAssignableFrom(t))
+                            {
+                                //Write("..."); WriteLine();
+                            }
+                            else
+                            {
+                                //Write("{ }"); WriteLine();
+                            }
+                        }
+                    }
+                }
+                foreach (MemberInfo m in members)
+                {
+                    FieldInfo? f = m as FieldInfo;
+                    PropertyInfo? p = m as PropertyInfo;
+                    if (f != null || p != null)
+                    {
+                        Type t = f != null ? f.FieldType : p.PropertyType;
+                        if (!(t.IsValueType || t == typeof(string)))
+                        {
+                            object? value = f != null ? f.GetValue(element) : p.GetValue(element, null);
+                            if (value != null)
+                            {
+                                WriteObject(w,prefix + m.Name + ": ", value);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void WriteValue(TextWriter w,object? o)
+    {
+        if (o == null) {
+            Write(w, "null");
+        }
+        else if (o is DateTime d) {
+            Write(w, d.ToShortDateString());
+        }
+        else if (o is YawPitchRoll ypr)
+        {
+            Write(w, ypr.Yaw.ToString()); Write(w, ":");
+            Write(w, ypr.Pitch.ToString()); Write(w, ":");
+            Write(w, ypr.Roll.ToString());
+        }
+        else if (o is ValueType || o is string)
+        {
+            Write(w,o.ToString());
+        }
+        else if (o is TechTreeBitMask ttbm)
+        {
+            // temporary fix.
+            BitArray bol = new(ttbm.bits.Length);
+            bol.SetAll(false);
+            int idx = 0;
+            foreach (bool b in ttbm.bits)
+            {
+                if (b)
+                    bol.Set(((idx / 8) * 8 + (7 - idx % 8)), true);
+                idx++;
+            }
+            // end of fix: to revert replace bol with ttbm.bits
+            idx = 0;
+            foreach (bool b in bol)
+            {
+                if (b)
+                    WriteValue(w, " " + idx);
+                idx++;
+            }
+        }
+        else if (o is IEnumerable)
+        {
+            Write(w, "...");
+        }
+        else
+        {
+            Write(w, "{ }");
+        }
+    }
+}
---
+++ igcexport/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IGCLib;
+using System.Reflection;
+
+namespace igcexport;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.Out.Write("error: no argument");
+            return;
+        }
+        IGCCore core = new();
+        core.Load(args[0]);
+        
+        //TODO: fancy print GAs attributes and other array values
+
+        ObjectDumper.Write("Globals-",core.Constants);
+
+        foreach (DataCivilizationIGC civ in core.m_civilizations)
+        {
+            string civid = "Factions-" + civ.name + "(" + civ.civilizationID+ ")-";
+            ObjectDumper.Write(civid,civ);
+        }
+        foreach (DataDevelopmentIGC dev in core.m_developments)
+        {
+            string devid = "Devels-"+dev.name+"("+dev.developmentID+")-";
+            ObjectDumper.Write(devid, dev);
+        }
+        foreach (DataDroneTypeIGC d in core.m_droneTypes)
+        {
+            string did = "Drones-" + d.name + " (" + d.droneTypeID + ")-";
+            ObjectDumper.Write(did, d);
+        }
+        foreach (DataProjectileTypeIGC p in core.m_projectileTypes)
+        {
+            string pid = "Projectiles-p#" + p.projectileTypeID +"-";
+            ObjectDumper.Write(pid, p);
+        }
+        foreach (DataStationTypeIGC s in core.m_stationTypes)
+        {
+            string sid = "Stations-" + s.name + "(" + s.stationTypeID + ")-";
+            ObjectDumper.Write(sid, s);
+        }
+        foreach (DataHullTypeIGC h in core.m_hullTypes)
+        {
+            string hid = "Ships-" + h.name + "(" + h.hullID + ")-";
+            ObjectDumper.Write(hid, h);
+        }
+        foreach (DataPartTypeIGC p in core.m_partTypes)
+        {
+            string pid = "Parts-" + p.name + "(" + p.partID + ")-";
+            ObjectDumper.Write(pid, p);
+        }
+        foreach (DataChaffTypeIGC m in core.m_chaffTypes)
+        {
+            string mid = "Chaffs-" + m.launcherDef.name + "(" + m.expendabletypeID + ")-";
+            ObjectDumper.Write(mid, m);
+        }
+        foreach (DataMineTypeIGC m in core.m_mineTypes)
+        {
+            string mid = "Mines-"+ m.launcherDef.name + "("+ m.expendabletypeID +")-";
+            ObjectDumper.Write(mid, m);
+        }
+        foreach (DataMissileTypeIGC m in core.m_missileTypes)
+        {
+            string mid = "Missiles-" + m.launcherDef.name + "(" + m.expendabletypeID + ")-";
+            ObjectDumper.Write(mid, m);
+        }
+        foreach (DataProbeTypeIGC m in core.m_probeTypes)
+        {
+            string mid = "Probes-" + m.launcherDef.name + "(" + m.expendabletypeID + ")-";
+            ObjectDumper.Write(mid, m);
+        }
+        foreach (DataLauncherTypeIGC l in core.m_launcherTypes)
+        {
+            string lid = "Launchers-" + l.partID + "-" + l.expendabletypeID+"-";
+            ObjectDumper.Write(lid, l);
+        }
+        foreach (DataTreasureSetIGC t in core.m_treasureSets)
+        {
+            string tid = "Treasures-" + t.name + "(" + t.treasureSetID + ")-";
+            ObjectDumper.Write(tid, t);
+        }
+    }
+}
